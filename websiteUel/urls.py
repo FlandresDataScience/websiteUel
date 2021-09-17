d5b9c9f8f25394aca4cad22d5186d96a78001de1
namaguederaz <ar.delattreabe@gmail.com>
2021-09-17 15:45:22 -0300

    pronto para editar templates e static files

--- websiteUel/urls.py
+++ websiteUel/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('spanishVersion/', include('spanishVersion.urls')),
+    path('', include('spanishVersion.urls')),
 ]
