aed3999d7f5f39f28f1df8eecfd4b43aaa411dd0
Anderson Abe <ander@arch.localdomain>
2021-11-12 16:40:25 -0300

    front imported/ some image issues

---
+++ spanishVersion/static/spanishVersion/Historia.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    setTimeout(function(){
+
+        // comentado para nao gerar log no console
+        // tem que colocar no html 
+        // $('#data-image').parallax({imageSrc: 'uel-construcao.jpg'});
+
+    }, 250);
+});
